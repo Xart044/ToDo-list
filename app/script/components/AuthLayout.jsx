923200429491d7a4514a84d055520bcb9a060a42
Olga Pistolkors <opistolkors@gmail.com>
2016-12-04 13:34:04 +0200

    Auth layout bg change

--- app/script/components/AuthLayout.jsx
+++ app/script/components/AuthLayout.jsx
@@ -13,7 +13,8 @@ export default class AuthLayout extends Component {
     render() {
         return (
             <div className="index-layout">
-                <h1 className="index-layout__title">Task Manager</h1>
+                <div className="index-layout__container">
+                    <h1 className="index-layout__title">Task Manager</h1>
                 <div className="index-layout__about">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem nemo ut in a nam quos minus ipsa officiis placeat quis, veniam, accusamus earum sunt nulla! Eos magnam in, deserunt ex reiciendis voluptatum! Atque doloribus repellat minus aliquam id dignissimos, officiis error rerum laboriosam, impedit eos maiores. Iusto autem, impedit quod.
                 </div>
@@ -33,7 +34,7 @@ export default class AuthLayout extends Component {
                             primary={true}
                         />
                     </Link>
-                </div>
+                </div></div>
             </div>
         );
     }
