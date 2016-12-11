eec296a562e96925b602d1d90e7a8b451c8198ac
Artem <xhc044x@gmail.com>
2016-12-11 21:19:44 +0200

    remove trash files

--- app/script/reducers/index.js
+++ app/script/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux'
-import page from './page'
 import user from './user'
 import tasks from './task'
 import category from './category'
 
 export default combineReducers(
     {
-        page,
         user,
         tasks,
         category
